554473dab94ab168fceddda978ac23d8b5d7ad10
Afif Bin Kamrul <afifbinkamrul@gmail.com>
2024-01-08 04:31:13 -0500

    Add files via upload

--- database/srms.sql
+++ database/srms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.6
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 10, 2018 at 04:35 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Jan 08, 2024 at 10:30 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin_login` (
   `userid` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `admin_login`
@@ -49,7 +48,7 @@ INSERT INTO `admin_login` (`userid`, `password`) VALUES
 CREATE TABLE `class` (
   `name` varchar(30) NOT NULL,
   `id` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -68,7 +67,32 @@ CREATE TABLE `result` (
   `p5` int(3) NOT NULL,
   `marks` int(3) NOT NULL,
   `percentage` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stu`
+--
+
+CREATE TABLE `stu` (
+  `stud_name` varchar(30) NOT NULL,
+  `stud_id` int(11) NOT NULL,
+  `dept_name` varchar(30) NOT NULL,
+  `level` int(11) NOT NULL,
+  `term` int(11) NOT NULL,
+  `course_title` varchar(30) NOT NULL,
+  `course_no` int(11) NOT NULL,
+  `grade` varchar(11) NOT NULL,
+  `point` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stu`
+--
+
+INSERT INTO `stu` (`stud_name`, `stud_id`, `dept_name`, `level`, `term`, `course_title`, `course_no`, `grade`, `point`) VALUES
+('audrey', 1, 'eee', 2, 2, 'comp6461', 6461, 'A', 4);
 
 -- --------------------------------------------------------
 
@@ -80,7 +104,26 @@ CREATE TABLE `students` (
   `name` varchar(30) NOT NULL,
   `rno` int(3) NOT NULL,
   `class_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `userid` int(11) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `dept` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`userid`, `password`, `dept`) VALUES
+(1, '123', 'eee');
 
 --
 -- Indexes for dumped tables
